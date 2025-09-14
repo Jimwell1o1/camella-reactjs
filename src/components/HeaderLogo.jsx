9f129822ccd6e923c25b2fd1acc33c2df1c4af90
Jimwell Rabino <jimwell.rabino@thynker.tech>
2025-09-15 02:42:06 +0800

    fix: update header logo link to remove external URL

--- src/components/HeaderLogo.jsx
+++ src/components/HeaderLogo.jsx
@@ -4,7 +4,7 @@ import IconLogo from "../assets/img/logo.png";
 const HeaderLogo = ({ isOpen, menuClick }) => {
     return (
         <nav className="sticky md:static top-0 z-50 md:justify-center lg:overflow-hidden overflow-auto flex bg-amber-50 lg:content-center lg:items-center justify-between items-center py-4 px-4 md:px-20 lg:px-16 xl:px-32 2xl:px-48">
-            <a href="https://dev5.primeoutsourcing.shop/tailwind/src">
+            <a href="#">
                 <img
                 src={IconLogo}
                 className="logo h-[4em] lg:h-[6em] xl:h-[7em]"
